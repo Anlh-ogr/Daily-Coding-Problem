888d1f5d431afcd54a515f2d916e5411546ae5f1
Anlh-morgan <9.10hoangan.13072003@gmail.com>
2024-07-24 13:30:52 +0700

    input function and bruteForce function
    problem 272 from Spotify

--- Problem_272_Medium/problem272_medium.js
+++ Problem_272_Medium/problem272_medium.js
@@ -6,8 +6,40 @@
 
 
 // input: 3 parameters
+function inputArguments() {
+    const numDice = parent(prompt("Enter the number of dice: "));
+    const faces = parent(prompt("Enter the number of faces: "));
+    const total = parent(prompt("Enter the total: "));
+
+    if(numDice < 1 || faces < 1 || total < 1) {
+        console.log("Invalid input");
+        return null;
+    }
+
+    return {numDice, faces, total};
+}
+
 
 // brute force solution
+function count_waysBruteForce(numDice, faces, total) {
+    if (numDice != 3) {
+        console.log("This implementation only supports exactly 3 dice for the brute force method.")
+        return;
+    }
+
+    let validCombinations = 0;
+
+    for (let firstDice = 1; firstDice <= faces; firstDice++) {
+        for (let secondDice = 1; secondDice <= faces; secondDice++) {
+            for (let thirdDice = 1; thirdDice <= faces; thirdDice++) {
+                if (firstDice + secondDice + thirdDice == total) {
+                    ++validCombinations;
+                }
+            }
+        }
+    }
+    console.log("The number of ways to get the total is: ", validCombinations);
+}
 
 // brute force method using macro
 
