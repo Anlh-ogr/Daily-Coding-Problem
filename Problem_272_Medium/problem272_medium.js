5ef96e42b0486cdb308be600fff5e166bbbc7ac5
Anlh-morgan <9.10hoangan.13072003@gmail.com>
2024-07-24 07:55:19 +0700

    crate a space for function

--- Problem_272_Medium/problem272_medium.js
+++ Problem_272_Medium/problem272_medium.js
@@ -1,5 +1,18 @@
 // Write a function, throw_dice(N, faces, total), that determines 
 // how many ways it is possible to throw N dice with some number of faces 
 // each to get a specific.
- 
+
 // For example, throw_dice(3, 6, 7) should equal 15.
+
+
+// input: 3 parameters
+
+// brute force solution
+
+// brute force method using macro
+
+// dynamic programming solution
+
+// recursion with memoization
+
+// main
